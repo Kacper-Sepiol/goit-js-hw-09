6d9c523ae3b7cd8650a6c2ca461ba07e431df33d
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-06-30 17:10:29 +0200

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+'use strict';
 
+const buttonStart = document.querySelector('.button-start');
+const buttonStop = document.querySelector('.button-stop');
+const body = document.querySelector('body');
+let randomBackgroundColor = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+buttonStart.addEventListener('click', () => {
+  randomBackgroundColor = setInterval(() => {
+    const backgroundColor = getRandomHexColor();
+    body.style.backgroundColor = backgroundColor;
+  }, 1000);
+});
