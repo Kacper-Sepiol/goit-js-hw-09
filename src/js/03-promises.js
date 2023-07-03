0d3c0375f87c3d1b2c4ed5b1310e24891b52bdfc
Kacper-Sepiol <kac-2008wp.pl@wp.pl>
2023-07-03 16:17:23 +0200

    update 03-promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const form = document.querySelector('.form');
+const inputDelay = document.querySelector('input[name="delay"]');
+const inputStep = document.querySelector('input[name="step]');
+const inputAmount = document.querySelector('input[name="amount]');
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
